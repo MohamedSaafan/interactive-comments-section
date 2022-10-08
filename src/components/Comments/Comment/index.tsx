1b3efad7dd86045a9b81b3c265104beda00a57d7
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2022-10-08 13:36:10 +0200

    started to use sass - included sass dependency to my project - fixed the line indentation of the nested replies roughly (more enhancements are comming in media queries)

--- src/components/Comments/Comment/index.tsx
+++ src/components/Comments/Comment/index.tsx
@@ -1,5 +1,5 @@
 import CommentCard from "../CommentCard";
-import Styles from "./comment.module.css";
+import Styles from "./comment.module.scss";
 import { Comment } from "../../../types";
 interface Props {
   comment: Comment;
@@ -9,6 +9,21 @@ const CommentComponent: React.FC<Props> = (props) => {
   return (
     <div className={Styles.commentContainer}>
       <CommentCard comment={props.comment} />
+      <div className={Styles.comment__replies}>
+        {props.comment.replies?.length ? (
+          <span className={Styles.comment__lineformatter}></span>
+        ) : (
+          ""
+        )}
+        {props.comment.replies?.map((comment) => {
+          return (
+            <div className={Styles.comment__nestingwrapper} key={comment.id}>
+              <span className={Styles.comment__line}> </span>
+              <CommentCard comment={comment} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
